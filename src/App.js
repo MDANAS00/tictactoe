8ead781c27ec82cd005eb60ffc6d5e137861d935
MDANAS00 <mohd44anas@gmail.com>
2023-07-14 17:29:36 +0530

    complete

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Board from './components/Board';
+
+function App() {
+  return (
+    <div>
+      <Board />
+    </div>
+  );
+}
+
+export default App;
+
